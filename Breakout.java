63acfdd2116bfa76f0cedd4fc6c1de91ac14feb9
S <s>
2013-02-01 00:02:04 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -67,8 +67,8 @@ public class Breakout extends GraphicsProgram {
 		for (int i = NBRICK_ROWS; i > 0; i--) {
 			buildBrickRow((WIDTH - (BRICK_WIDTH * NBRICKS_PER_ROW) - (BRICK_SEP * (NBRICKS_PER_ROW - 1))) / 2, 
 					BRICK_Y_OFFSET, Color.RED);
-			buildBrickRow((WIDTH - (BRICK_WIDTH * NBRICKS_PER_ROW) - (BRICK_SEP * (NBRICKS_PER_ROW - 1))) / 2, 
-					BRICK_Y_OFFSET + height * (BRICK_SEP + BRICK_HEIGHT), Color.RED);
+			//buildBrickRow((WIDTH - (BRICK_WIDTH * NBRICKS_PER_ROW) - (BRICK_SEP * (NBRICKS_PER_ROW - 1))) / 2, 
+				//	BRICK_Y_OFFSET + height * (BRICK_SEP + BRICK_HEIGHT), Color.RED);
 			//buildBrickRow((WIDTH - (BRICK_WIDTH * NBRICKS_PER_ROW) - (BRICK_SEP * (NBRICKS_PER_ROW - 1))) / 2, 
 				//	BRICK_Y_OFFSET + 2 * (BRICK_SEP + BRICK_HEIGHT), Color.ORANGE);
 			height ++;
