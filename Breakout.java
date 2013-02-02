e0134fec8e6048ea4ba324bceddc3ee4d1412bdc
S <s>
2013-02-01 16:48:00 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -75,7 +75,7 @@ public class Breakout extends GraphicsProgram {
 			               color);
 			height ++;
 			if (i > 2 && i < 4) {
-				color = Color.ORANGE;
+				color = Color.YELLOW;
 			} 
 			if (i > 4 && i < 6) {
 				color = Color.GREEN;
@@ -83,8 +83,8 @@ public class Breakout extends GraphicsProgram {
 			if (i > 6 && i < 8) {
 				color = Color.CYAN;
 			}		
-			if (i > 8 && i < 10) {
-				color = Color.YELLOW;
+			if (i > 0 && i < 2) {
+				color = Color.ORANGE;
 			} 
 			}
 	}
