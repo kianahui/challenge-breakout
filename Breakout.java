e2e79f2991b75189bd3aef909581472b05dd17ef
S <s>
2013-02-04 02:30:22 -0800

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -33,7 +33,7 @@ public class Breakout extends GraphicsProgram {
 	private static final int PADDLE_Y_OFFSET = 30;
 
 	/** Number of bricks per row */
-	private static final int NBRICKS_PER_ROW = 10;
+	private static final int NBRICKS_PER_ROW = 3;
 
 	/** Number of rows of bricks */
 	private static final int NBRICK_ROWS = 10;
