b413e724d6ff846491498406b3c4c0e2042a2742
S <s>
2013-02-04 23:12:36 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -98,7 +98,7 @@ public class Breakout extends GraphicsProgram {
 	
 	private boolean isBallAboveCeiling(GOval ball) {
 		double topOfBall = ball.getY();
-		return topOfBall <= HEIGHT;
+		return topOfBall >= HEIGHT;
 	}
 	
 	private boolean isBallOffRight(GOval ball) {
