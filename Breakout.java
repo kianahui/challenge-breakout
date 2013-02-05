ad9cf09b5c724c03a75a3e9e6fbef50d556a5dca
S <s>
2013-02-05 00:50:53 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -88,30 +88,25 @@ public class Breakout extends GraphicsProgram {
 				vx = -vx;
 			} 
 			moveBall();
-			getCollidingObject();
-			if (collider != null) {
-				vy = -vy;
-				moveBall();
-			}
 		}
-
+		GObject collider = getCollidingObject();
+		if (collider != null) {
+			vy = -vy;
+			moveBall();
+		}
 	}
 	
 	public GObject getElementAt(double x, double y) {
 		return ball;
 	}
 	
-	private GObject collider;
 	
 	private GObject getCollidingObject() {
-		collider = getElementAt(ball.getX(), ball.getY());
-		collider = getElementAt(ball.getX() + BALL_DIAMETER, ball.getY());
-		collider = getElementAt(ball.getX(), ball.getY() + BALL_DIAMETER);
-		collider = getElementAt(ball.getX() + BALL_DIAMETER, ball.getY() + BALL_DIAMETER);
-		//GObject collider = getCollidingObject();
-		return collider;
-		
-
+		getElementAt(ball.getX(), ball.getY());
+		getElementAt(ball.getX() + BALL_DIAMETER, ball.getY());
+		getElementAt(ball.getX(), ball.getY() + BALL_DIAMETER);
+		getElementAt(ball.getX() + BALL_DIAMETER, ball.getY() + BALL_DIAMETER);
+		return ball;
 	}
 	
 	private boolean isBallAboveCeiling(GOval ball) {
