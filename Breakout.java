bc61ac7df2eb8c66cc818bcfbf9fca4a4a39abae
S <s>
2013-02-04 23:13:23 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -84,8 +84,8 @@ public class Breakout extends GraphicsProgram {
 			} else if (isBallAboveCeiling(ball)) {
 				vy = -vy;
 				//moveBall();
-			//} else if (isBallOffRight(ball)) {
-			//	vx = -vx;
+			} else if (isBallOffRight(ball)) {
+				vx = -vx;
 				//moveBall();
 			//} else if (isBallOffLeft(ball)) {
 				//vx = -vx;
