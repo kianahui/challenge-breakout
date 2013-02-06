cf47bb3c7fd2d513ccf2b2fd9c29f168cfbd8578
S <s>
2013-02-06 13:05:57 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -95,8 +95,9 @@ public class Breakout extends GraphicsProgram {
 			GObject collider = getCollidingObject();
 			if (collider != null) {
 				vy = -vy;
-				moveBall();
+				//moveBall();
 			}
+			moveBall();
 		}
 
 
