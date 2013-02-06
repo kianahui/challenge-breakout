9ac86a18b8a24abcec213ffe74a0b624081711f6
S <s>
2013-02-06 13:28:46 -0800

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -99,7 +99,7 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	public GObject getElementAt(double x, double y) {
-		return ball;
+		return getElementAt(x, y);
 	}
 
 	private GObject getCollidingObject() {
