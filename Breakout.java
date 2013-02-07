797c4aadc54fd1d7ef5f3ebac88f42b17763b649
S <s>
2013-02-06 18:44:38 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -96,15 +96,16 @@ public class Breakout extends GraphicsProgram {
 			}
 			//if(turnsRemaining == 0) {	
 			//}
-			removeAll();
-			GLabel label = new GLabel("GAMEOVER!");
-			label.setFont("SansSerif-28");
-			label.setColor(Color.RED);
-			double x = (getWidth() - label.getWidth()) / 2;
-			double y = (getHeight() + label.getAscent()) / 2;
-			label.setLocation(x, y);
-			add(label);
+			
 		}
+		removeAll();
+		GLabel label = new GLabel("GAMEOVER!");
+		label.setFont("SansSerif-28");
+		label.setColor(Color.RED);
+		double x = (getWidth() - label.getWidth()) / 2;
+		double y = (getHeight() + label.getAscent()) / 2;
+		label.setLocation(x, y);
+		add(label);
 	}
 
 	private void checkForCollisions() {
