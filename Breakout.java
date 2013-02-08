adedef4463ca554f87ec97671cd5b14493cd482c
S <s>
2013-02-08 03:27:29 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -218,8 +218,8 @@ public class Breakout extends GraphicsProgram {
 			GLabel label = new GLabel("You had to destroy " + bricksRemaining + "more bricks!");
 			label.setFont("Arial-BOLD-15");
 			label.setColor(Color.ORANGE);
-			double x = (getWidth() + label.getWidth()) / 2;
-			double y = ((getHeight() + label.getWidth()) / 2) + 30;
+			double x = (getWidth() - label.getWidth()) / 2;
+			double y = ((getHeight() - label.getWidth()) / 2) + 10;
 			label.setLocation(x, y);
 			add(label);
 			gameLostClip.play();
