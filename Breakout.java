3827c6913b53b21f016d98aa6a9907ee819eb37c
S <s>
2013-02-07 22:26:05 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -213,7 +213,7 @@ public class Breakout extends GraphicsProgram {
 			} else {
 				remove(collider);
 				bounceClip.play();
-				vy = -vy;
+				vy = -Math.abs(vy);
 				bricksRemaining--;
 			}
 			/*if (bricksRemaining < ((NBRICK_ROWS * NBRICKS_PER_ROW) / 2)) {
