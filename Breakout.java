138294c98d92cd57996aa503f34c77f6a508975d
S <s>
2013-02-08 03:29:40 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -219,7 +219,7 @@ public class Breakout extends GraphicsProgram {
 			label.setFont("Arial-BOLD-15");
 			label.setColor(Color.ORANGE);
 			double x = (getWidth() - label.getWidth()) / 2;
-			double y = (getHeight()  / 2) + 10;
+			double y = (getHeight()  / 2) + 20;
 			label.setLocation(x, y);
 			add(label);
 			gameLostClip.play();
