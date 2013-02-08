cce915018723046a293fcb9bbd886494e08ff7f0
S <s>
2013-02-07 23:32:20 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -151,7 +151,7 @@ public class Breakout extends GraphicsProgram {
 					displayTurns("Click for new ball. " + turnsRemaining + " turns left.");
 				}
 				if (turnsRemaining == 1) {
-					displayTurns("Click for new ball. " + turnsRemaining + " turn left.");
+					displayTurns("Click for new ball. " + turnsRemaining + " turn left!! LAST LIFE.");
 				}
 			}
 		}
